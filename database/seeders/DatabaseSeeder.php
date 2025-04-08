629d4464d90de3163d39dfc49e558b8380e0bb6e
ricomuh <rico.smartlolo@gmail.com>
2025-04-08 20:25:08 +0700

    Refactor DatabaseSeeder to create an Admin user and seed posts with random views

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Post;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -16,8 +17,22 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'Admin',
+            'email' => 'admin@example.com',
         ]);
+
+        Post::factory(30)->create([
+            'user_id' => 1,
+            'created_at' => now()->subDays(rand(1, 30)),
+        ])->each(
+            function ($post) {
+                for ($i = 0; $i < rand(1, 10); $i++) {
+                    $post->views()->create([
+                        'views' => rand(1, 100),
+                        'date' => now()->subDays(rand(1, 30)),
+                    ]);
+                }
+            }
+        );
     }
 }
