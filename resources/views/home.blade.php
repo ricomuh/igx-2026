620f041a6d90dc7af6381266680d580173551d3d
khafidhasukma <khafidhasukma@gmail.com>
2025-04-09 09:54:24 +0700

    feat: setup assets

---
+++ resources/views/home.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.main', [
+    'title' => 'Home',
+])
+@section('content')
+    <div class="flex flex-col items-center justify-center h-screen">
+        <h1 class="text-4xl font-bold mb-4">Welcome to the Home Page</h1>
+        <p class="text-lg">This is a simple example of a Blade template.</p>
+    </div>
+@endsection
---
+++ resources/views/layouts/partials/style.blade.php
@@ -0,0 +1,12 @@
+{{-- Meta tag --}}
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1" />
+<title>{{ env('APP_NAME') }} {{ @$title ? '| ' . $title : '' }}</title>
+<meta charset="utf-8" />
+<meta name="description" content="{{ env('APP_NAME') }}">
+<meta name="author" content="{{ env('APP_NAME') }}">
+<meta name="robots" content="noindex, nofollow">
+
+@if (file_exists(public_path('build/manifest.json')) || file_exists(public_path('hot')))
+    @vite(['resources/css/app.css'])
+@endif
