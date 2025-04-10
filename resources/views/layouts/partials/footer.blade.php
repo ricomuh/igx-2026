be463faea7b80e360b566199d06704155afb7a2f
khafidhasukma <khafidhasukma@gmail.com>
2025-04-10 10:32:54 +0700

    fix: update wave illustration and adjust footer layout for improved design

--- resources/views/layouts/partials/footer.blade.php
+++ resources/views/layouts/partials/footer.blade.php
@@ -1,5 +1,5 @@
-<img src="{{ asset('media/images/illustrations/wave.svg')}}" class="w-full -mt-32" alt="">
-<div class="bg-background-footer -mt-6 pb-8 text-white">
+<img src="{{ asset('media/images/illustrations/wave.svg')}}" class="w-full -mt-40" alt="">
+<div class="bg-background-footer pb-8 text-white">
     <div class="container mx-auto px-12">
         {{-- Header Section --}}
         <div class="flex gap-4 justify-between items-center mb-20">
@@ -15,7 +15,7 @@
         </div>
 
         {{-- Footer Links and Social Media --}}
-        <div class="flex justify-between items-center gap-5">
+        <div class="flex justify-between items-center gap-5 relative">
             {{-- Navigation Links --}}
             <ul class="flex flex-col gap-1">
                 @foreach (['Home' => route('home'), 'Guests' => '#', 'Rundown' => '#', 'Exhibitors' => '#', 'News' => '#'] as $name => $link)
@@ -26,19 +26,21 @@
             </ul>
 
             {{-- Logo and Social Media --}}
-            <div class="flex flex-col gap-6">
-                <img src="{{ asset('/media/images/logos/logo.svg') }}" class="h-12" alt="Logo">
-                <div class="flex gap-8">
-                    @foreach ([
-                        'whatsapp' => 'https://api.whatsapp.com/message/U3XML62HR7O2C1',
-                        'instagram' => 'https://www.instagram.com/indonesiagameexpo/',
-                        'facebook' => 'https://www.facebook.com/share/uxMivasQaUMuc5fZ/?mibextid=LQQJ4d',
-                        'youtube' => 'https://www.youtube.com/@indonesiagameexpo'
-                    ] as $platform => $url)
-                        <a href="{{ $url }}" target="_blank" class="hover:scale-110 transition-transform duration-300">
-                            <img src="{{ asset("/media/images/icons/{$platform}-logo.svg") }}" class="size-6" alt="{{ ucfirst($platform) }}">
-                        </a>
-                    @endforeach
+            <div class="absolute left-0 -transform-x-1/2 w-full">
+                <div class="flex flex-col gap-6">
+                    <img src="{{ asset('/media/images/logos/logo.svg') }}" class="h-12" alt="Logo">
+                    <div class="flex gap-8 justify-center">
+                        @foreach ([
+                            'whatsapp' => 'https://api.whatsapp.com/message/U3XML62HR7O2C1',
+                            'instagram' => 'https://www.instagram.com/indonesiagameexpo/',
+                            'facebook' => 'https://www.facebook.com/share/uxMivasQaUMuc5fZ/?mibextid=LQQJ4d',
+                            'youtube' => 'https://www.youtube.com/@indonesiagameexpo'
+                        ] as $platform => $url)
+                            <a href="{{ $url }}" target="_blank" class="hover:scale-110 transition-transform duration-300">
+                                <img src="{{ asset("/media/images/icons/{$platform}-logo.svg") }}" class="size-6" alt="{{ ucfirst($platform) }}">
+                            </a>
+                        @endforeach
+                    </div>
                 </div>
             </div>
 
