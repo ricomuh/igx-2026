367a652d7d5fbf994d1a460dcb9470cfa729f438
khafidhasukma <khafidhasukma@gmail.com>
2025-04-19 22:49:13 +0700

    feat: add recommended posts section to article detail view and update styling

--- resources/views/components/news/latest.blade.php
+++ resources/views/components/news/latest.blade.php
@@ -26,7 +26,7 @@
                     <img src="{{ $news->image_url }}" alt="{{ $news->title }}" class="aspect-video h-40 object-cover rounded-2xl m-4 group-hover:scale-105 transition duration-300" />
                     <div class="p-4">
                         <h2 class="text-2xl font-extrabold mb-2 transition group-hover:text-background-footer duration-300">{{ Str::limit($news->title, 35, '...') }}</h2>
-                        <p class="group-hover:text-background-footer mb-2 opacity-90 text-lg transition duration-300">{{ Str::limit($news->body, 75, '...') }}</p>
+                        <p class="group-hover:text-background-footer mb-2 opacity-90 text-lg transition duration-300">{{ Str::limit($news->body, 70, '...') }}</p>
                         <div class="flex gap-2 items-center opacity-90">
                             <img src="{{ asset('media/images/icons/calendar.svg') }}" class="w-4" alt="">
                             <p class="font-medium">{{ \Carbon\Carbon::parse($news->created_at)->format('d M Y') }}</p>
--- resources/views/news/show.blade.php
+++ resources/views/news/show.blade.php
@@ -4,7 +4,7 @@
 
 @section('content')
 <section id="detail-article">
-    <div class="container px-12 mx-auto mt-20 pb-40">
+    <div class="container px-12 mx-auto mt-20 pb-32">
         {{-- Article Image --}}
         <div class="relative mb-6 overflow-hidden rounded-lg md:mb-8 md:max-h-96 lg:mb-10 lg:max-h-[30rem] xl:mb-12">
             <img
@@ -21,6 +21,10 @@
 
         {{-- Article Title --}}
         <h1 class="text-center text-4xl font-extrabold my-5">{{ $post->title }}</h1>
+        <div class="flex items-center gap-2 opacity-90 justify-center mb-5">
+            <img src="{{ asset('media/images/icons/calendar.svg') }}" class="size-5" alt="Calendar">
+            <p class="font-medium text-lg">{{ \Carbon\Carbon::parse($post->created_at)->format('d M Y') }}</p>
+        </div>
 
         {{-- Article Body --}}
         <p class="text-lg">{{ $post->body }}</p>
@@ -67,6 +71,31 @@
                     <path d="M380.9 97.1C339 55.1 283.2 32 223.9 32c-122.4 0-222 99.6-222 222 0 39.1 10.2 77.3 29.6 111L0 480l117.7-30.9c32.4 17.7 68.9 27 106.1 27h.1c122.3 0 224.1-99.6 224.1-222 0-59.3-25.2-115-67.1-157zm-157 341.6c-33.2 0-65.7-8.9-94-25.7l-6.7-4-69.8 18.3L72 359.2l-4.4-7c-18.5-29.4-28.2-63.3-28.2-98.2 0-101.7 82.8-184.5 184.6-184.5 49.3 0 95.6 19.2 130.4 54.1 34.8 34.9 56.2 81.2 56.1 130.5 0 101.8-84.9 184.6-186.6 184.6zm101.2-138.2c-5.5-2.8-32.8-16.2-37.9-18-5.1-1.9-8.8-2.8-12.5 2.8-3.7 5.6-14.3 18-17.6 21.8-3.2 3.7-6.5 4.2-12 1.4-32.6-16.3-54-29.1-75.5-66-5.7-9.8 5.7-9.1 16.3-30.3 1.8-3.7 .9-6.9-.5-9.7-1.4-2.8-12.5-30.1-17.1-41.2-4.5-10.8-9.1-9.3-12.5-9.5-3.2-.2-6.9-.2-10.6-.2-3.7 0-9.7 1.4-14.8 6.9-5.1 5.6-19.4 19-19.4 46.3 0 27.3 19.9 53.7 22.6 57.4 2.8 3.7 39.1 59.7 94.8 83.8 35.2 15.2 49 16.5 66.6 13.9 10.7-1.6 32.8-13.4 37.4-26.4 4.6-13 4.6-24.1 3.2-26.4-1.3-2.5-5-3.9-10.5-6.6z" />
                 </svg>
             </a>
+        </div>
+
+        {{-- Read More --}}
+        <div class="mt-8">
+            <h1 class="font-extrabold text-3xl mb-8">Read More</h1>
+            {{-- List News --}}
+            <div class="grid grid-cols-2 gap-8">
+                @forelse ($recommended_posts as $news)
+                    <a href="{{ route('news.show', ['post' => $news->slug]) }}" class="block">
+                        <div class="flex items-center hover:bg-white/20 rounded-2xl overflow-hidden group hover:shadow-lg transition duration-300">
+                            <img src="{{ $news->image_url }}" alt="{{ $news->title }}" class="aspect-video h-40 object-cover rounded-2xl m-4 group-hover:scale-105 transition duration-300" />
+                            <div class="p-4">
+                                <h2 class="text-2xl font-extrabold mb-2 transition group-hover:text-background-footer duration-300">{{ Str::limit($news->title, 35, '...') }}</h2>
+                                <p class="group-hover:text-background-footer mb-2 opacity-90 text-lg transition duration-300">{{ Str::limit($news->body, 70, '...') }}</p>
+                                <div class="flex gap-2 items-center opacity-90">
+                                    <img src="{{ asset('media/images/icons/calendar.svg') }}" class="w-4" alt="">
+                                    <p class="font-medium">{{ \Carbon\Carbon::parse($news->created_at)->format('d M Y') }}</p>
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                @empty
+                    <p class="text-center col-span-2 text-lg font-medium text-gray-500">No news found.</p>
+                @endforelse
+            </div>
         </div>
     </div>
 </section>
