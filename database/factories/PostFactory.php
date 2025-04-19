fc7b1f365fbe053151719991efa7385aa9f5dad3
ricomuh <rico.smartlolo@gmail.com>
2025-04-19 22:40:29 +0700

    feat: update PostFactory to generate HTML body with multiple sections

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -18,10 +18,28 @@ class PostFactory extends Factory
     {
         return [
             'title' => $this->faker->sentence,
-            'body' => $this->faker->paragraph,
+            'body' => $this->generateHtmlBody(),
             'slug' => fn($table) => str($table['title'])->slug(),
             'image_url' => $this->faker->imageUrl(640, 480, 'cats', true),
             'user_id' => 1,
         ];
+    }
+
+    /**
+     * Generate an HTML body with 3 sections.
+     *
+     * @return string
+     */
+    private function generateHtmlBody(): string
+    {
+        $html = '';
+        for ($i = 1; $i <= 3; $i++) {
+            $html .= "<section>";
+            $html .= "<h2>Section {$i}</h2>";
+            $html .= "<p>" . $this->faker->paragraph . "</p>";
+            $html .= "<p>" . $this->faker->paragraph . "</p>";
+            $html .= "</section>";
+        }
+        return $html;
     }
 }
