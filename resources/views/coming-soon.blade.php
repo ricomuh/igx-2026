ae67bbb6091a7b01aafe01f20008737186491418
khafidhasukma <khafidhasukma@gmail.com>
2025-05-14 16:14:13 +0700

    feat: enhance coming soon page with responsive scaling for better visibility

--- resources/views/coming-soon.blade.php
+++ resources/views/coming-soon.blade.php
@@ -4,7 +4,7 @@
 @section('content')
 <main class="flex h-screen text-center items-center justify-center bg-primary text-white relative overflow-hidden">
     <img src="{{ asset('media/images/illustrations/banner.webp') }}" class="w-full opacity-5" alt="">
-    <div class="absolute">
+    <div class="absolute scale-40 sm:scale-50 md:scale-60 xl:scale-80 2xl:scale-100">
         <div class="container mx-auto px-12">
             <h1 class="font-extrabold text-[8.5rem] tracking-[-1rem]">
                 WE ARE
