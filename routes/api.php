44c13204f4875d7519a94b60a1d228314f173fd7
ricomuh <rico.smartlolo@gmail.com>
2025-06-04 17:39:01 +0700

    feat: add API route for storing scores in ScoreController

--- routes/api.php
+++ routes/api.php
@@ -6,3 +6,7 @@ use Illuminate\Support\Facades\Route;
 // Route::get('/user', function (Request $request) {
 //     return $request->user();
 // })->middleware('auth:sanctum');
+
+Route::prefix('v1')->as('api.v1')->group(function () {
+    Route::post('/scores', [App\Http\Controllers\Api\V1\ScoreController::class, 'store']);
+});
