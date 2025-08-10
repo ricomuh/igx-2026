30fb62cac07cd69ba1f2bc02163cf6ae31cbb28b
Khafidha Sukma <khafidhasukma@gmail.com>
2025-08-10 18:20:59 +0700

    fix: improve layout and styling of the play section in the homepage

--- resources/views/components/home/play.blade.php
+++ resources/views/components/home/play.blade.php
@@ -1,15 +1,15 @@
-<section class="container px-5 mx-auto py-20 text-white">
+<section class="container px-5 mx-auto lg:px-12 xl:px-20 py-20 text-white">
   <div class="flex flex-col items-center text-center justify-center gap-4 mb-10">
     <img src="{{ asset('media/images/logos/logo.png')}}" class="h-24" alt="">
-    <h2 class="text-4xl">PLAY THE <span class="text-secondary">IGX FUSION CELEBRATION,</span>
+    <h2 class="text-2xl md:text-3xl lg:text-4xl">PLAY THE <span class="text-secondary">IGX FUSION CELEBRATION,</span>
       <br>WIN <span class="text-secondary">FREE TICKETS!</span></h2>
   </div>
-  <div class="grid grid-cols-2 gap-12 items-center">
-    <div class="col">
-      <img src="{{ asset('media/images/illustrations/game1.jpg')}}" class="w-full h-full object-cover rounded-2xl ring-5 ring-background-footer" alt="">
+  <div class="grid lg:grid-cols-2 gap-8 md:gap-12 items-center">
+    <div class="col lg:h-full">
+      <img src="{{ asset('media/images/illustrations/game1.jpg')}}" class="w-full md:aspect-video lg:aspect-auto h-full object-cover rounded-2xl ring-5 ring-background-footer" alt="">
     </div>
     <div class="col space-y-5">
-      <ul class="list-disc pl-5 text-xl leading-9">
+      <ul class="list-disc pl-5 text-lg md:text-xl leading-9">
         <li><span class="text-info">Free ticket every Monday at 10:00 AM WIB</span> for the top winners on the leaderboard.</li>
         <li><span class="text-info">Terms & Conditions</span>: Previous winners are not eligible to win again.</li>
         <li><span class="text-info">3 winners every Monday</span> - will you be next?</li>
