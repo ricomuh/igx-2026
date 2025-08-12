afea44e46e7a1fabdfeffe21430a8bcf160d448f
ricomuh <rico.smartlolo@gmail.com>
2025-08-12 13:22:58 +0700

    fix: rename variable for clarity in ExperienceController to improve code readability

--- app/Http/Controllers/ExperienceController.php
+++ app/Http/Controllers/ExperienceController.php
@@ -10,11 +10,11 @@ class ExperienceController extends Controller
     public function __invoke(Request $request)
     {
         // get the top 10 scores created in this week starting in monday at 10am
-        $scores = Score::where('created_at', '>=', now()->startOfWeek()->addHours(10))
+        $leaderboard = Score::where('created_at', '>=', now()->startOfWeek()->addHours(10))
             ->orderBy('score', 'desc')
             ->take(10)
             ->get();
 
-        return view('experience.index', compact('scores'));
+        return view('experience.index', compact('leaderboard'));
     }
 }
