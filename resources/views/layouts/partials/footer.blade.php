f6400f9e4f0ece0008f33c803dd112ed03e74f29
ricomuh <rico.smartlolo@gmail.com>
2025-09-27 14:20:52 +0700

    fix: update ticket link in footer to direct to the correct URL

--- resources/views/layouts/partials/footer.blade.php
+++ resources/views/layouts/partials/footer.blade.php
@@ -9,7 +9,7 @@
             </h1>
             <div class="flex flex-col gap-4 w-full sm:w-80">
                 <a href="https://www.instagram.com/indonesiagameexpo/" target="_blank" class="btn-primary text-center font-extrabold text-base xl:text-lg px-6 py-3 sm:px-12 sm:py-4 rounded-lg uppercase">Join Us</a>
-                <a href="{{ route('promo') }}" class="btn-primary text-center font-extrabold text-base xl:text-lg px-6 py-3 sm:px-12 sm:py-4 rounded-lg uppercase">Get Ticket</a>
+                <a href="http://blib.li/igx-2025" class="btn-primary text-center font-extrabold text-base xl:text-lg px-6 py-3 sm:px-12 sm:py-4 rounded-lg uppercase">Get Ticket</a>
             </div>
         </div>
 
